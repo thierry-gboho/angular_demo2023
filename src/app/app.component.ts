2b10713ee356a7fbfffdfa28e9e4d800d0528d54
Thierry Gboho <tmhgboho4@yahoo.com>
2025-09-11 13:05:58 +0200

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
-export class AppComponent {
-  title = 'angular_demo2023_standalone';
-}
+export class AppComponent {}
