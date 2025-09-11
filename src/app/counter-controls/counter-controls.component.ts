097b4957a947227b79646707f36da725f4b0b9f4
Thierry Gboho <tmhgboho4@yahoo.com>
2025-09-11 23:52:21 +0200

    dispatching an action to update the store data

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-import { CounterService } from '../counter.service';
+import { increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +10,24 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    /*
+    * Dispatch the increment action defined in ../store/counter.actions
+    * Notice that an action is actually a function and the dispatch method
+    * takes as argument the executed function:
+    * We DO NOT write
+    *         this.store.dispatch(increment);
+    * but
+    *         this.store.dispatch(increment());
+    *
+    * so that it is executed when dispatching
+    */
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    this.counterService.decrement();
+
   }
 }
