46f5cee1973a2496567c0061222967e1c1de066a
Thierry Gboho <tmhgboho4@yahoo.com>
2025-09-11 21:25:50 +0200

    an alternative way of creating a reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './store/counter.reducer';
+import { counterReducerV2 } from './store/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { counterReducer } from './store/counter.reducer';
     CounterControlsComponent,
   ],
   imports: [BrowserModule, StoreModule.forRoot({
-    counter: counterReducer
+    counter: counterReducerV2
   }, {})],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -7,11 +7,32 @@ import { createReducer } from "@ngrx/store";
 const initialState = 0;
 
 /*
-* A reducer is created with at least one parameter: the initial state
+*
 * Here we create a simple reducer for our counter.
 * It's not too useful yet because it does not contain the logic to modify
 * our state (i.e. the logic to increment our counter)
+*
+* counterReducer and counterReducerV2 are identical and for now look quite similar
+* but we'll see some difference later when we'll start adding some logic to the
+* reducer
+*/
+
+/*
+* A reducer is created with at least one parameter: the initial state
 */
 export const counterReducer = createReducer(
   initialState
 );
+
+
+/*
+* The alternative approach to create a reducer is to define a function
+* which takes as first parameter the current state and returns the updated state
+* 1. This approach works in all versions of NgRx
+* 2. we set the default value of the state to be the initial state as the first
+*    time this function is called there is no state. As a result, the first time
+*    this function is called it returns the initial state
+*/
+export function counterReducerV2(state = initialState) {
+  return state;
+}
